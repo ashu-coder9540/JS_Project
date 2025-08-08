5cd178ea4158aa2dcf1a55b6ebf9db51377485f8
ashu-coder9540 <ashu954008@gmail.com>
2025-08-08 15:47:11 +0530

    Add Currency Converter implementation with HTML, CSS, and JavaScript

---
+++ Currency-Converter/script.js
@@ -0,0 +1,71 @@
+const BASE_URL = "https://api.frankfurter.app";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("#btn");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+
+for(let select of dropdowns){
+    for(let currCode in countryList){
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+     if(select.name === "from" && currCode === "USD"){
+        newOption.selected = "selected";
+    } else if(select.name === "to" && currCode === "INR"){
+        newOption.selected = "selected";
+    }
+    select.append(newOption); 
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    })
+}
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}/latest?from=${fromCurr.value}&to=${toCurr.value}`;
+    let response = await fetch(URL);
+
+    if (!response.ok) {
+        alert("Currency pair not supported by the API.");
+        return;
+    }
+
+    let data = await response.json();
+    if (!data.rates || !data.rates[toCurr.value]) {
+        alert("Conversion rate not available for this currency pair.");
+        return;
+    }
+
+    let rate = data.rates[toCurr.value];
+    
+    let finalAmt = amtVal*rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img  = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
